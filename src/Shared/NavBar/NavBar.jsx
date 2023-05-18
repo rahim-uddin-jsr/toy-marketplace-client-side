f7a0ccd6a5c4de2f567aed5481ca80f27262bd51
dev-rahim <rahimuddin5915@gmail.com>
2023-05-19 01:09:53 +0600

    navbar desine added done with active route and responsiveness

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,5 +1,105 @@
+import { Link, NavLink } from "react-router-dom";
+
 const NavBar = () => {
-  return <div>nav</div>;
+  const navItems = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li tabIndex={0}>
+        <NavLink
+          to="all-toys"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          All Toys
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="my-toys"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          My Toys
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="add-toys"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Add A Toy
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="blogs"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          {" "}
+          Blogs
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2  shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </ul>
+        </div>
+        <Link
+          to="/"
+          className="btn btn-ghost normal-case text-sm sm:text-2xl sm:font-extrabold"
+        >
+          = <span className="text-primary"> KIDDO </span> ZONE=
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <Link to="/login" className="btn sm:btn btn-sm">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 import Main from "./Layout/Main/Main";
+import AddToys from "./Pages/AddToys/AddToys";
+import AllToys from "./Pages/AllToys/AllToys";
+import Blogs from "./Pages/Blogs/Blogs";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login";
+import MyToys from "./Pages/MyToys/MyToys";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/all-toys", element: <AllToys /> },
+      { path: "/add-toys", element: <AddToys /> },
+      { path: "/my-toys", element: <MyToys /> },
+      { path: "/blogs", element: <Blogs /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
