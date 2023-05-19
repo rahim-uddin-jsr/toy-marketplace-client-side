563162eb3d1fe017087b269550017687b8a4921d
dev-rahim <rahimuddin5915@gmail.com>
2023-05-19 07:55:59 +0600

    banner section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,9 @@
+import Banner from "./Banner/Banner";
+
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      <div className="btn-group">
-        <button className="btn btn-active">Button</button>
-        <button className="btn">Button</button>
-        <button className="btn">Button</button>
-      </div>
+      <Banner></Banner>
     </div>
   );
 };
