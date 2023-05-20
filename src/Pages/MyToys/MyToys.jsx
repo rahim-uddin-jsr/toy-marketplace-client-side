9cbffb0370872536979d7b6b4fa8b4bd13652095
dev-rahim <rahimuddin5915@gmail.com>
2023-05-20 16:29:51 +0600

    my toy page added with tabluer from data with delete and update button

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,8 +1,42 @@
-
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
+import MyToyRow from "./MyToyRow";
 
 const MyToys = () => {
- 
-  return <div></div>;
+  const { user } = useContext(AuthContext);
+  const [toys, setToys] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/toys/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, [user]);
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr className="text-center">
+              <th>Seller Name</th>
+              <th>Toy Name</th>
+              <th>Toy Picture</th>
+              <th>Sub-category</th>
+              <th>Price</th>
+              <th>Available Quantity</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {toys?.map((toy) => (
+              <MyToyRow key={toy?._id} toy={toy} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyToys;
